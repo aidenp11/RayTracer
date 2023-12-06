1f1f70d5a2629c2909783bdd30efc82f72ec5c0f
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2023-12-06 12:12:13 -0700

    initscenechangesandotherstuffs

--- Source/RayTracer/Scene.cpp
+++ Source/RayTracer/Scene.cpp
@@ -2,10 +2,13 @@
 #include "Canvas.h"
 #include "MathUtils.h"
 #include "Random.h"
+#include "Mesh.h"
+#include "Sphere.h"
+#include "Plane.h"
 #include <iostream>
 #include <iomanip>
 
-void Scene::Render(Canvas& canvas, int numSamples)
+void Scene::Render(Canvas& canvas, int numSamples, int depth)
 {
 	// cast ray for each point (pixel) on the canvas
 	for (int y = 0; y < canvas.GetSize().y; y++)
@@ -30,13 +33,14 @@ void Scene::Render(Canvas& canvas, int numSamples)
 				// cast ray into scene
 				// set color value from trace
 				raycastHit_t raycastHit;
-				color += Trace(ray, 0, 100, raycastHit, m_depth);
+				color += Trace(ray, 0, 100, raycastHit, depth);
 			}
 				//color3_t color = Trace(ray);
 			color /= numSamples;
 			// draw color to canvas point (pixel)
 			canvas.DrawPoint(pixel, color4_t(color, 1));
 		}
+		std::cout << std::setprecision(2) << std::setw(5) << (((float)y / (float)canvas.GetSize().y) * 100) << "%\n";
 	}
 }
 //color3_t Scene::Trace(const ray_t& ray)
@@ -90,4 +94,68 @@ color3_t Scene::Trace(const ray_t& ray, float minDistance, float maxDistance, ra
 	color3_t color = lerp(m_bottomColor, m_topColor, t);
 
 	return color;
+}
+
+void Scene::InitScene01(Scene& scene, const Canvas& canvas)
+{
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+
+	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0.4f });
+	
+	auto plane = std::make_unique<Plane>(glm::vec3{ 0, -0.2f, 0 }, glm::vec3{ 0, 1, 0 }, lambertian);
+	scene.AddObject(std::move(plane));
+
+	scene.SetCamera(camera);
+
+	for (int x = -10; x < 10; x++)
+	{
+		for (int z = -10; z < 10; z++)
+		{
+
+			std::shared_ptr<Material> material;
+
+			// create random material
+			float r = random01();
+			if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
+			else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
+			else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
+			else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
+
+			// set random sphere radius
+			float radius = random(0.2f, 0.3f);
+			// create sphere using random radius and material
+			auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f) }, radius, material);
+			// add sphere to the scene
+			scene.AddObject(std::move(sphere));
+		}
+	}
+}
+
+void Scene::InitScene02(Scene& scene, const Canvas& canvas)
+{
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+
+	scene.SetCamera(camera);
+
+	auto mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0, 0, 1 }));
+	mesh->Load("models/cube.obj", glm::vec3{ 0, 0.2f, 0 }, glm::vec3{ 0, 45, 0 });
+	scene.AddObject(std::move(mesh));
+}
+
+void Scene::InitScene03(Scene& scene, const Canvas& canvas)
+{
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+
+	scene.SetCamera(camera);
+
+	auto floor = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 1, 1 }));
+	floor->Load("models/quaad.obj", glm::vec3{ 0, 0.3, 7.3 }, glm::vec3{ -90, 0, 0 }, glm::vec3{ 1.2, 2, 1 });
+	scene.AddObject(std::move(floor));
+
+	auto ceiling = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 1, 1, 1 }));
+	ceiling->Load("models/quaad.obj", glm::vec3{ 0, 0.7, 7.3 }, glm::vec3{ 90, 0, 0 }, glm::vec3{ 1.2, 2, 1 });
+	scene.AddObject(std::move(ceiling));
 }
